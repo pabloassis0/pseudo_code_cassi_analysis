3661557e9d62a428568754916f6455d554309bec
Pablo Borges <89882590+pabloassis0@users.noreply.github.com>
2021-11-03 09:38:57 -0300

    aaaa

--- list_3.js
+++ list_3.js
@@ -2,6 +2,9 @@
 THIS IS A PSEUDO CODE, ONLY FOR GUIDING PURPOSES
 */
 
+//Getting all the data from our database, about services and costs of that
+var iron_cassi_glossed = query_on_our_data_base.where(client==cassi || client==bem_cassi || client==cassi_df).where(services)
+
 //Getting all the data from the spreadsheet 
 var cassi_spreadsheet = import_and_open(cassi_spreadsheet.xlsx)
 var items = read_all_columns(cassi_spreadsheet) 
@@ -11,8 +14,8 @@ listGlossed = [];
 // verify if the column glossed exists
 // here, will also need to get what was the reason of glossed
 forEach (item in items);{ 
-    if(item.glossed === TRUE){
-        listGlossed.append_to_it(item)
+    if(item.glossed === iron_cassi_glossed){
+        listGlossed.append_to_it(item) // New line, even if it's repeted
     }
 } 
 
