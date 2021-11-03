47eb80ca7de58fb57f41135752862af9ec8125e5
Pablo Borges <89882590+pabloassis0@users.noreply.github.com>
2021-11-02 23:29:11 -0300

    Create list_5.js

---
+++ list_5.js
@@ -0,0 +1,20 @@
+/*
+THIS IS A PSEUDO CODE, ONLY FOR GUIDING PURPOSES
+*/
+
+//Getting all the data from our database
+cassi_users = query_on_our_data_base.where(client==cassi || client==bem_cassi || client==cassi_df)
+
+//Getting all the data from the spreadsheet
+cassi_spreadsheet = import_and_open(cassi_spreadsheet.xlsx)
+items = read_all_columns(cassi_spreadsheet) 
+
+listOfNotFound = []
+
+forEach (item in items){ 
+    if(item.name is not on cassi_users){
+       listOfNotFound.append_to_it(item)
+    }
+} 
+
+return listOfNotFound //this can be empty, in case all the registry on the cassi_spreadsheet exists on our database
